917e9520599fdcc61c3eebf36566eefeb007de4c
tomast1337 <vycasnicolas@gmail.com>
2022-08-11 21:14:12 -0300

    Melhorias no ConfigReader

--- ConfigReader.cs
+++ ConfigReader.cs
@@ -5,34 +5,42 @@ namespace Program
     class ConfigReader
     {
         // Data from the config.json
-        static public Dictionary<string, string> config = new Dictionary<string, string>();
+        static private Dictionary<string, string> config = new Dictionary<string, string>();
         // Flag indicating if the config file was read
-        static bool configRead = false;
+        static private bool configRead = false;
+
 
         // Reads the config.json file and returns a dictionary with the configuration
         public static Dictionary<string, string> ReadConfig()
         {
             if (configRead)
                 return config;
-            var necessaryKeys = new List<string> { "PrimaryDomain",
-                                                   "PrimaryPort",
-                                                   "UsernameEmail",
-                                                   "UsernamePassword",
-                                                   "DestinationEmail" };
-            
+            var necessaryKeys = new List<string>
+            {
+                "AlphaVantageAPIKEY",
+                "CheckInterval",
+                "SMTPserver",
+                "SMTPname",
+                "SMTPusername",
+                "SMTPpassword",
+                "SMTPportTLS",
+                "SMTPportSSL",
+                "DestinationEmail"
+            };
+
             var configFile = File.ReadAllText("config.json");
-            
+
             var configJson = JsonConvert.DeserializeObject<Dictionary<string, string>>(configFile);
-            
+
             foreach (var key in necessaryKeys)
             {
                 if (!configJson.ContainsKey(key))
                     throw new Exception($"The config.json file is missing the key {key}");
                 ConfigReader.config.Add(key, configJson[key]);
             }
-            
+
             configRead = true;
-            
+
             return config;
         }
     }
