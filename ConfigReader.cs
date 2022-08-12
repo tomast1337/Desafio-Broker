a1235dd9ab5dd4cd06de9879b2b653429515962e
tomast1337 <vycasnicolas@gmail.com>
2022-08-11 22:06:46 -0300

    Removido campo não utilizado das configurações

--- ConfigReader.cs
+++ ConfigReader.cs
@@ -20,7 +20,6 @@ namespace Program
                 "AlphaVantageAPIKEY",
                 "CheckInterval",
                 "SMTPserver",
-                "SMTPname",
                 "SMTPusername",
                 "SMTPpassword",
                 "SMTPportTLS",
