86f2c76fbb42b61aa7705921eb9bbef05dafea66
tomast1337 <vycasnicolas@gmail.com>
2022-08-14 19:03:53 -0300

    Removido comentário

--- Program.cs
+++ Program.cs
@@ -1,28 +1,4 @@
-﻿/*
-## Requisitos
-
-O objetivo do sistema é avisar, via e-mail, caso a cotação de um ativo da B3 caia mais do que certo nível, ou suba acima de outro.
-
-O programa deve ser uma aplicação de console (não há necessidade de interface gráfica).
-
-Ele deve ser chamado via linha de comando com 3 parâmetros.
-
-O ativo a ser monitorado
-O preço de referência para venda
-O preço de referência para compra
-Ex.
-
-`stock-quote-alert.exe PETR4 22.67 22.59` 
-
-Ele deve ler de um arquivo de configuração com:
-
-O e-mail de destino dos alertas
-As configurações de acesso ao servidor de SMTP que irá enviar o e-mail
-A escolha da API de cotação é livre.
-
-O programa deve ficar continuamente monitorando a cotação do ativo enquanto estiver rodando.
-*/
-namespace Program
+﻿namespace Program
 {
     class programa
     {
